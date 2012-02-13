082e7217dafbceddfff3257012738fe91731c50c
Jurg van Vliet <jurg@truthtrap.com>
2012-02-13 12:27:15 +0000

    tuned for c1.xlarge

--- start.py
+++ start.py
@@ -98,6 +98,7 @@ def set_conf():
 	bucket = userdata['cluster'].replace('.', '-')
 	conf = "{0}/etc/postgresql/9.1/main/{1}.conf".format(path, instance_type)
 	os.system("cp {0} {1}".format(conf, pg_conf))
+	os.system("/bin/chown postgres.postgres {0}".format(pg_conf))
 	os.system("/bin/sed -i \x27s_s3://[^/]*/_s3://{0}/_\x27 {1}".format(bucket, pg_conf))
 
 def set_recovery_conf():
