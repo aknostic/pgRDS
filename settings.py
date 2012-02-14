152b6e2630cd23679ce76c0264561b81ceae2f4b
Jurg van Vliet <jurg@truthtrap.com>
2012-02-14 07:19:38 +0000

    some fixes for start/stop and tuning for m2.4xlarge

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
-settings.host = "localhost"
-settings.database_name = "fashiolista"
-settings.database_user = "postgres"
-settings.database_password = "YomQaRgI6tkfeslh6p1uOpNsspV6eL6n"
+host = "localhost"
+database_name = "fashiolista"
+database_user = "postgres"
+database_password = "YomQaRgI6tkfeslh6p1uOpNsspV6eL6n"
