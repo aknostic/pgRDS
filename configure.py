0b18f8f0366b753f4025e1452547246393e5562e
Jurg van Vliet <jurg@truthtrap.com>
2012-03-02 15:49:18 +0000

    godver

--- configure.py
+++ configure.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
 						os.environ['HOSTED_ZONE_NAME'].rstrip('.'))
 
 	if sys.argv[3] == "start":
-		#r53_zone.create_record(name, hostname)
+		r53_zone.create_record(name, hostname)
 		ec2.create_tags([instance_id], { "Name": name })
 
 		# we only prepare the database when we are NOT subservient
