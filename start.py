17d3993c78e44be36559f764ff82e3d461bdaa82
Jurg van Vliet <jurg@truthtrap.com>
2012-06-18 14:05:52 +0000

    will this work?

--- start.py
+++ start.py
@@ -150,8 +150,7 @@ def set_recovery_conf():
 	
 	if clone != None:
 		f.write("recovery_target_time = '{0}'\n".format(timestamp))
-		f.write("pause_at_recovery_target = false\n")
-		f.write("recovery_end_command = '/etc/init.d/pgbouncer restart'\n")
+		f.write("recovery_end_command = 'sudo -u postgres /usr/lib/postgresql/9.1/bin/psql postgres -c \"select pg_xlog_replay_resume();\" && /etc/init.d/pgbouncer restart'\n")
 
 	# don't know if/how this works
 	#f.write("recovery_target_timeline = latest\n")
